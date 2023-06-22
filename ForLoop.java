c21726f66e0a82a61fc470183bb77b6df9c782c1
ripandeep1750 <71696207+ripandeep1750@users.noreply.github.com>
2023-06-22 18:22:12 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,8 @@
+public class forLoop{
+    public static void main(String args[]){
+        int x;
+        for(x=0;x<10;x=x+1){
+            System.out.println("Value of x is: " +x);
+        }
+    }
+}
