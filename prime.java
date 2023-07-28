e9012cbe236453d41b36d3018fe472e9fa80996c
ripandeep1750 <71696207+ripandeep1750@users.noreply.github.com>
2023-07-28 18:25:56 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,16 @@
+class Main{
+    public static void main(String args[]){
+        int num=12;
+        boolean flag=false;
+        for(int i=2; i<=num/2; i++){  
+//  a number can not divisible by more than half of the number hence num/2
+            if(num%i==0)
+            flag=true;
+            break;
+        }
+        if(!flag)
+        System.out.println(num + " is a prime number.");
+        else
+        System.out.println(num + " is not a prime number.");
+    }
+}
